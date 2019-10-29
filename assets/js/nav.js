a4ef934e809375968fc45d3d6d1fad2fdfd897f7
heavylildude <heavylildude@gmail.com>
2019-10-29 12:18:35 +0700

    Add files via upload

---
+++ assets/js/nav.js
@@ -0,0 +1,12 @@
+var nav = '<div id="main_menu">&#9776;</div>' +
+    '<div id="menu_container">' +
+      '<div class="menu_header">' +
+        '<div class="user_icon"></div>' +
+        '<div class="username">Whatever</div>' +
+      '</div>' +
+
+      '<div class="each_menu">Home</div>' +
+      '<div class="each_menu">About</div>' +
+      '<div class="each_menu">Contact</div>' +
+    '</div>' +
+    '<div id="close_menu"></div>';
---
+++ assets/js/top_part.js
@@ -0,0 +1,8 @@
+var top_part = '<div class="one_col">' +
+      '<div class="card">' +
+        '<div class="card_image" style="background-image:url(\'assets/bitmaps/1.jpg\')"></div>' +
+        '<div class="card_title">Full Screen Wide Card</div>' +
+        '<div class="card_text">Lorem ipsum dolor consectetur adipisicing elit, dolor consectetur ipsum</div>' +
+       ' <button type="button">MORE</button>' +
+      '</div>' +
+    '</div>';
